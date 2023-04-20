095a0e5edad12e82282ab0fce45570b3d9170d10
YutoTerashima <jixu3640@colorado.edu>
2023-04-20 11:01:39 -0600

    update database, profile page and profile api

--- All project code/index.js
+++ All project code/index.js
@@ -161,17 +161,24 @@ app.post('/login', async (req, res) => {
 });
 
 
-app.get("/profile", (req, res) => {
-  res.render("pages/profile", {
-    username: req.session.user.username,
-    first_name: req.session.user.first_name,
-    last_name: req.session.user.last_name,
-    email: req.session.user.email,
-    year: req.session.user.year,
-    major: req.session.user.major,
-    degree: req.session.user.degree,
-  });
+app.get('/profile', async (req, res) => {
+  try {
+    const user = req.session.user;
+    const favorite_books = await Book.findAll({ 
+      where: { id: { [Op.in]: Sequelize.literal(`(SELECT book_id FROM user_to_book WHERE user_id = ${user.id})`) } } 
+    });
+    const my_comments = await Comment.findAll({ 
+      where: { user_id: user.id } 
+    });
+    res.render('pages/profile', { user, favorite_books, my_comments });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error');
+  }
 });
+
+
+
 
 app.get('/register', (req, res) => {
   res.render("pages/register");
--- All project code/init_data/create.sql
+++ All project code/init_data/create.sql
@@ -1,7 +1,60 @@
+-- Drop tables if they exist
+DROP TABLE IF EXISTS user_to_books CASCADE;
+DROP TABLE IF EXISTS user_to_comments CASCADE;
+DROP TABLE IF EXISTS books_to_comments CASCADE;
+DROP TABLE IF EXISTS books CASCADE;
+DROP TABLE IF EXISTS comments CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users(
+
+-- Create the users table
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
     username VARCHAR(50) PRIMARY KEY,
-    password CHAR(60) NOT NULL
+    password CHAR(60) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    year INT NOT NULL,
+    major VARCHAR(255) NOT NULL,
+    degree VARCHAR(255) NOT NULL
+);
+
+-- Create the user_to_books table
+CREATE TABLE user_to_books (
+    user_id INT REFERENCES users(id),
+    book_id INT REFERENCES books(id),
+    PRIMARY KEY(user_id, book_id)
+);
+
+-- Create the user_to_comments table
+CREATE TABLE user_to_comments (
+    user_id INT REFERENCES users(id),
+    comment_id INT REFERENCES comments(id),
+    PRIMARY KEY(user_id, comment_id)
+);
+
+-- Create the books_to_comments table
+CREATE TABLE books_to_comments (
+    book_id INT REFERENCES books(id),
+    comment_id INT REFERENCES comments(id),
+    PRIMARY KEY(book_id, comment_id)
+);
+
+-- Create the books table
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    genre VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL
+);
+
+-- Create the comments table
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    book_id INT REFERENCES books(id),
+    user_id VARCHAR(50) REFERENCES users(username),
+    comment TEXT NOT NULL
 );
 
 INSERT INTO users (username, password) VALUES ('admin','$2b$10$OobnoGdMm7qtw4mq5Eqv/Oxbf1NJECNpfbTBxyrakBB2AtvqEDRK6');
