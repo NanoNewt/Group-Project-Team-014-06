4c1b2fdf1efab0f0bbce0feb73a293d16dbcf2db
YutoTerashima <jixu3640@colorado.edu>
2023-04-20 11:27:19 -0600

    update API

--- All project code/index.js
+++ All project code/index.js
@@ -171,10 +171,10 @@ app.post('/login', async (req, res) => {
 app.get('/profile', async (req, res) => {
   try {
     const user = req.session.user;
-    const favorite_books = await Book.findAll({ 
+    const favorite_books = await books.findAll({ 
       where: { id: { [Op.in]: Sequelize.literal(`(SELECT book_id FROM user_to_book WHERE user_id = ${user.id})`) } } 
     });
-    const my_comments = await Comment.findAll({ 
+    const my_comments = await comments.findAll({ 
       where: { user_id: user.id } 
     });
     res.render('pages/profile', { user, favorite_books, my_comments });
