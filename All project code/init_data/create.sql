0ca83f3ff7f9fe13d49c58c3d84ef32f16e293c0
NanoNewt <quinn@nicodemus.name>
2023-04-24 14:27:18 -0600

    added romeo and juiliet page 1 for testing purposes

--- All project code/init_data/create.sql
+++ All project code/init_data/create.sql
@@ -81,3 +81,64 @@ CREATE TABLE books_to_annotation (
 
 INSERT INTO users (username, password) VALUES ('test','test');
 INSERT INTO users (username, password) VALUES ('admin','$2b$10$OobnoGdMm7qtw4mq5Eqv/Oxbf1NJECNpfbTBxyrakBB2AtvqEDRK6');
+INSERT INTO books (id, title, author, genre, description) VALUES (1513,'Romeo and Juliet','William Shakespeare','?','description');
+INSERT INTO book_pages (book_id, page_number, page_content) VALUES ('1513',1,'The Project Gutenberg eBook of Romeo and Juliet, by William Shakespeare
+
+This eBook is for the use of anyone anywhere in the United States and
+most other parts of the world at no cost and with almost no restrictions
+whatsoever. You may copy it, give it away or re-use it under the terms
+of the Project Gutenberg License included with this eBook or online at
+www.gutenberg.org. If you are not located in the United States, you
+will have to check the laws of the country where you are located before
+using this eBook.
+
+Title: Romeo and Juliet
+
+Author: William Shakespeare
+
+Release Date: November, 1998 [eBook #1513]
+[Most recently updated: May 11, 2022]
+
+Language: English
+
+Character set encoding: UTF-8
+
+Produced by: the PG Shakespeare Team, a team of about twenty Project Gutenberg volunteers.
+
+*** START OF THE PROJECT GUTENBERG EBOOK ROMEO AND JULIET ***
+
+
+
+
+THE TRAGEDY OF ROMEO AND JULIET
+
+
+
+by William Shakespeare
+
+
+Contents
+
+THE PROLOGUE.
+
+ACT I
+Scene I. A public place.
+Scene II. A Street.
+Scene III. Room in Capulet’s House.
+Scene IV. A Street.
+Scene V. A Hall in Capulet’s House.
+
+
+ACT II
+CHORUS.
+Scene I. An open place adjoining Capulet’s Garden.
+Scene II. Capulet’s Garden.
+Scene III. Friar Lawrence’s Cell.
+Scene IV. A Street.
+Scene V. Capulet’s Garden.
+Scene VI. Friar Lawrence’s Cell.
+
+
+ACT III
+Scene I. A public Place.
+Scene II. A Room in Capulet’s House.');
